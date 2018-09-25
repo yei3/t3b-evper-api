c3161f202c21cffd190efad1a9b73628b06788db
luisangel <luisangel@yei3.com>
2018-09-24 23:28:58 -0500

    Auth JWT CRUD

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+namespace Evaluation.API.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string email { get; set; }
+        public string level { get; set; }
+    }
+}
