87a71b994d2b97202286f2566cd0068d99bc8aec
edgeroy <edgeroy@p1stache.com>
2018-10-16 17:58:36 -0500

    added default roles seeds

--- src/Yei3.PersonalEvaluation.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
+++ src/Yei3.PersonalEvaluation.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
@@ -27,6 +27,7 @@ namespace Yei3.PersonalEvaluation.EntityFrameworkCore.Seed
             // Default tenant seed (in host database).
             new DefaultTenantBuilder(context).Create();
             new TenantRoleAndUserBuilder(context, 1).Create();
+            new DefaultRoleBuilder(context, 1).Create();
         }
 
         private static void WithDbContext<TDbContext>(IIocResolver iocResolver, Action<TDbContext> contextAction)
---
+++ src/Yei3.PersonalEvaluation.EntityFrameworkCore/EntityFrameworkCore/Seed/Tenants/DefaultRoleBuilder.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Yei3.PersonalEvaluation.Authorization.Roles;
+
+namespace Yei3.PersonalEvaluation.EntityFrameworkCore.Seed.Tenants
+{
+    class DefaultRoleBuilder
+    {
+        private readonly PersonalEvaluationDbContext _context;
+        private readonly int _tenantId;
+
+        public DefaultRoleBuilder(PersonalEvaluationDbContext context, int tenantId)
+        {
+            _context = context;
+            _tenantId = tenantId;
+        }
+
+        public void Create()
+        {
+            CreateDefaultRoles();
+        }
+
+        private void CreateDefaultRoles()
+        {
+            // Default roles
+
+            Role collaboratorRole = _context.Roles.IgnoreQueryFilters().FirstOrDefault(role => role.Name == StaticRoleNames.Tenants.Collaborator);
+            if (collaboratorRole == null)
+            {
+                collaboratorRole = new Role(_tenantId, StaticRoleNames.Tenants.Collaborator, StaticRoleNames.Tenants.Collaborator);
+                Role administratorRole = new Role(_tenantId, StaticRoleNames.Tenants.Administrator, StaticRoleNames.Tenants.Administrator);
+                Role supervisorRole = new Role(_tenantId, StaticRoleNames.Tenants.Supervisor, StaticRoleNames.Tenants.Supervisor);
+
+                _context.Roles.Add(collaboratorRole);
+                _context.Roles.Add(administratorRole);
+                _context.Roles.Add(supervisorRole);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
