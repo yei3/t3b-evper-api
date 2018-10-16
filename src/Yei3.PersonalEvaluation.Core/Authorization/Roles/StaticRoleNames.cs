fec48ba5f9ee6a2c7dec6842c86534e4114099a5
edgeroy <edgeroy@p1stache.com>
2018-10-16 17:58:15 -0500

    added biz roles

--- src/Yei3.PersonalEvaluation.Core/Authorization/Roles/StaticRoleNames.cs
+++ src/Yei3.PersonalEvaluation.Core/Authorization/Roles/StaticRoleNames.cs
@@ -9,7 +9,10 @@ namespace Yei3.PersonalEvaluation.Authorization.Roles
 
         public static class Tenants
         {
-            public const string Admin = "Admin";
+            public const string Admin = "Admin"; // Admin stands for system admin
+            public const string Administrator = "Admnistrator";
+            public const string Supervisor = "Supervisor";
+            public const string Collaborator = "Collaborator";
         }
     }
 }
