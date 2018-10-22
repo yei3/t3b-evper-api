06bbed5c98e8fa55c8d702819d7dfd35155a85dc
edgeroy <edgeroy@p1stache.com>
2018-10-22 02:17:31 -0500

    added permission for evaluation creation

--- src/Yei3.PersonalEvaluation.Core/Authorization/PersonalEvaluationAuthorizationProvider.cs
+++ src/Yei3.PersonalEvaluation.Core/Authorization/PersonalEvaluationAuthorizationProvider.cs
@@ -11,6 +11,8 @@ namespace Yei3.PersonalEvaluation.Authorization
             context.CreatePermission(PermissionNames.Pages_Users, L("Users"));
             context.CreatePermission(PermissionNames.Pages_Roles, L("Roles"));
             context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"), multiTenancySides: MultiTenancySides.Host);
+
+            context.CreatePermission(PermissionNames.AdministrationEvaluationCreate, L("CreateEvaluationPermission"));
         }
 
         private static ILocalizableString L(string name)
