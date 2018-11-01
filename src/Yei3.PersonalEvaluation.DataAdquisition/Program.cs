db78a92877cb6993b014dc0d31cd1ebd2579c766
edgeroy <edgeroy@p1stache.com>
2018-10-31 23:48:27 -0500

    treated repeated users

--- src/Yei3.PersonalEvaluation.DataAdquisition/Program.cs
+++ src/Yei3.PersonalEvaluation.DataAdquisition/Program.cs
@@ -26,7 +26,6 @@ namespace Yei3.PersonalEvaluation.DataAdquisition
                 StringBuilder stringBuilder = new StringBuilder();
                 ExcelWorksheet worksheet = package.Workbook.Worksheets["Hoja1"];
                 int rowCount = worksheet.Dimension.Rows;
-                int ColCount = worksheet.Dimension.Columns;
 
                 List<User> importedUsers = new List<User>();
 
@@ -38,24 +37,33 @@ namespace Yei3.PersonalEvaluation.DataAdquisition
 
                     for (int row = 3; row < rowCount; row++) // start in row 3 cause data starts there, any template change can break this, is better if we provide the template
                     {
-                        User currentUser = userRegistrationManager.ImportUser(
-                            employeeNumber: worksheet.Cells[row, 1].Value.ToString(),
-                            status: worksheet.Cells[row, 2].Value.ToString() == AppConst.IsActiveImportValue,
-                            firstLastName: worksheet.Cells[row, 3].Value.ToString(),
-                            secondLastName: worksheet.Cells[row, 4].Value.ToString(),
-                            name: worksheet.Cells[row, 5].Value.ToString(),
-                            jobDescription: worksheet.Cells[row, 6].Value.ToString(),
-                            area: worksheet.Cells[row, 7].Value.ToString(),
-                            region: worksheet.Cells[row, 8].Value.ToString(),
-                            immediateSupervisor: worksheet.Cells[row, 9].Value.ToString(),
-                            socialReason: worksheet.Cells[row, 10].Value.ToString(),
-                            role: worksheet.Cells[row, 11].Value.ToString(),
-                            entryDate: worksheet.Cells[row, 12].Value.ToString(),
-                            reassignDate: worksheet.Cells[row, 13].Value == null ? null : worksheet.Cells[row, 13].Value.ToString(),
-                            birthDate: worksheet.Cells[row, 14].Value.ToString(),
-                            scholarship: worksheet.Cells[row, 15].Value.ToString(),
-                            email: worksheet.Cells[row, 16].Value.ToString()
+                        try
+                        {
+                            User currentUser = userRegistrationManager.ImportUser(
+                                employeeNumber: worksheet.Cells[row, 1].Value.ToString(),
+                                status: worksheet.Cells[row, 2].Value.ToString() == AppConst.IsActiveImportValue,
+                                firstLastName: worksheet.Cells[row, 3].Value.ToString(),
+                                secondLastName: worksheet.Cells[row, 4].Value.ToString(),
+                                name: worksheet.Cells[row, 5].Value.ToString(),
+                                jobDescription: worksheet.Cells[row, 6].Value.ToString(),
+                                area: worksheet.Cells[row, 7].Value.ToString(),
+                                region: worksheet.Cells[row, 8].Value.ToString(),
+                                immediateSupervisor: worksheet.Cells[row, 9].Value.ToString(),
+                                socialReason: worksheet.Cells[row, 10].Value.ToString(),
+                                role: worksheet.Cells[row, 11].Value.ToString(),
+                                entryDate: worksheet.Cells[row, 12].Value.ToString(),
+                                reassignDate: worksheet.Cells[row, 13].Value == null
+                                    ? null
+                                    : worksheet.Cells[row, 13].Value.ToString(),
+                                birthDate: worksheet.Cells[row, 14].Value.ToString(),
+                                scholarship: worksheet.Cells[row, 15].Value.ToString(),
+                                email: worksheet.Cells[row, 16].Value.ToString()
                             );
+                        }
+                        catch (Exception)
+                        {
+                            System.Console.WriteLine($"Usuario {worksheet.Cells[row, 3].Value} fue agregado anteriormente.");
+                        }
                     }
                 }
             }
