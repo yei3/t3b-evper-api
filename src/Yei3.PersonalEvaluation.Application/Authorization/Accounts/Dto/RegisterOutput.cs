7ac73031e87878b655322cef7c555231d6abe8b2
edgeroy <edgeroy@p1stache.com>
2018-11-03 11:47:50 -0500

    added errors to first time login feature response

--- src/Yei3.PersonalEvaluation.Application/Authorization/Accounts/Dto/RegisterOutput.cs
+++ src/Yei3.PersonalEvaluation.Application/Authorization/Accounts/Dto/RegisterOutput.cs
@@ -1,7 +1,11 @@
-﻿namespace Yei3.PersonalEvaluation.Authorization.Accounts.Dto
+﻿using System.Collections.Generic;
+
+namespace Yei3.PersonalEvaluation.Authorization.Accounts.Dto
 {
     public class RegisterOutput
     {
+        public bool HasErrors { get; set; }
         public bool CanLogin { get; set; }
+        public List<string> Errors { get; set; }
     }
 }
