9c0428826d51fd8973013f74fe2fef864138485b
edgeroy <edgeroy@p1stache.com>
2018-12-18 02:22:46 -0500

    fixed missing dto

--- src/Yei3.PersonalEvaluation.Application/EvaluationTemplate/Dto/SectionDto.cs
+++ src/Yei3.PersonalEvaluation.Application/EvaluationTemplate/Dto/SectionDto.cs
@@ -2,6 +2,7 @@
 using Abp.Application.Services.Dto;
 using Abp.AutoMapper;
 using JetBrains.Annotations;
+using Yei3.PersonalEvaluation.Objective.Dto;
 
 namespace Yei3.PersonalEvaluation.EvaluationTemplate.Dto
 {
@@ -13,7 +14,7 @@ namespace Yei3.PersonalEvaluation.EvaluationTemplate.Dto
         public long? EvaluationTemplateId { get; set; }
         public long? ParentId { get; set; }
         [CanBeNull] public ICollection<QuestionDto> UnmeasuredQuestions { get; set; }
-        [CanBeNull] public ICollection<QuestionDto> MeasuredQuestions { get; set; }
+        [CanBeNull] public ICollection<ObjectiveDto> MeasuredQuestions { get; set; }
         [CanBeNull] public ICollection<SectionDto> ChildSections { get; set; }
     }
 }
