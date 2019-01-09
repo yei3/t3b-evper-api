e9d7d254176a6c1fc8ddeb8330ffddce5f9978c3
edgeroy <edgeroy@p1stache.com>
2019-01-08 19:51:56 -0500

    add evaluation objective id to get all binnacle feature

---
+++ src/Yei3.PersonalEvaluation.Application/Binnacle/Dto/ObjectiveBinnacleGetAllInputDTO.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace Yei3.PersonalEvaluation.Binnacle.Dto
+{
+    public class ObjectiveBinnacleGetAllInputDto : PagedAndSortedResultRequestDto
+    {
+        public long EvaluationMeasuredQuestionId { get; set; }
+    }
+}
