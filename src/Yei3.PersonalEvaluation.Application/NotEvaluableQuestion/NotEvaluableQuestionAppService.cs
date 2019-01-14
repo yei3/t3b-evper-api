84e2a5ec62ba92c0b5f2f84973879d2684586fe1
edgeroy <edgeroy@p1stache.com>
2019-01-14 13:28:09 -0500

    add answer creation feature

--- src/Yei3.PersonalEvaluation.Application/NotEvaluableQuestion/NotEvaluableQuestionAppService.cs
+++ src/Yei3.PersonalEvaluation.Application/NotEvaluableQuestion/NotEvaluableQuestionAppService.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Application.Services;
+﻿using System.Threading.Tasks;
+using Abp.Application.Services;
 using Abp.Domain.Repositories;
 using Yei3.PersonalEvaluation.NotEvaluableQuestion.Dto;
 using Yei3.PersonalEvaluation.Question.Dto;
@@ -9,6 +10,18 @@ namespace Yei3.PersonalEvaluation.NotEvaluableQuestion
     {
         public NotEvaluableQuestionAppService(IRepository<Evaluations.EvaluationQuestions.NotEvaluableQuestion, long> repository) : base(repository)
         {
+        }
+
+        public override async Task<NotEvaluableQuestionDto> Create(NotEvaluableQuestionDto input)
+        {
+            NotEvaluableQuestionDto notEvaluableQuestion = await base.Create(input);
+            await CurrentUnitOfWork.SaveChangesAsync();
+
+            Evaluations.EvaluationQuestions.NotEvaluableQuestion currentQuestion = await Repository.SingleAsync(question => question.Id == notEvaluableQuestion.Id);
+
+            currentQuestion.SetAnswer(notEvaluableQuestion.Id);
+
+            return notEvaluableQuestion;
         }
     }
 }
