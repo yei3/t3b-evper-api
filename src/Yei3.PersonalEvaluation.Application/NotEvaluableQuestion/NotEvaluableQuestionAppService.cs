6a3023bef51f174f690e7b1483a3cf89cc8867b6
edgeroy <edgeroy@p1stache.com>
2019-01-15 10:11:37 -0500

    new feature

--- src/Yei3.PersonalEvaluation.Application/NotEvaluableQuestion/NotEvaluableQuestionAppService.cs
+++ src/Yei3.PersonalEvaluation.Application/NotEvaluableQuestion/NotEvaluableQuestionAppService.cs
@@ -1,7 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Abp.Application.Services;
 using Abp.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Yei3.PersonalEvaluation.Evaluations.EvaluationQuestions;
+using Yei3.PersonalEvaluation.Evaluations.ValueObject;
 using Yei3.PersonalEvaluation.NotEvaluableQuestion.Dto;
 using Yei3.PersonalEvaluation.Question.Dto;
 
@@ -27,6 +32,32 @@ namespace Yei3.PersonalEvaluation.NotEvaluableQuestion
             await CurrentUnitOfWork.SaveChangesAsync();
 
             return MapToEntityDto(currentQuestion);
+        }
+
+        public async Task<List<EvaluationObjectivesSummaryValueObject>> GetSummary(long evaluationId)
+        {
+            return await Repository
+                  .GetAll()
+                  .Include(evaluationQuestion => evaluationQuestion.Evaluation)
+                  .Where(evaluationQuestion => evaluationQuestion.Evaluation.Id == evaluationId)
+                  .Where(evaluationQuestion => evaluationQuestion.Status != EvaluationQuestionStatus.Validated)
+                  .Where(evaluationQuestion => evaluationQuestion.Evaluation.EndDateTime > DateTime.Now)
+                  .OfType<Evaluations.EvaluationQuestions.NotEvaluableQuestion>()
+                  .Select(evaluationQuestion => new EvaluationObjectivesSummaryValueObject
+                  {
+                      Status = evaluationQuestion.Status,
+                      Name = evaluationQuestion.Text,
+                      DeliveryDate = evaluationQuestion.NotEvaluableAnswer.CommitmentTime,
+                      Id = evaluationQuestion.Id,
+                      Binnacle = evaluationQuestion.Binnacle.Select(objectiveBinnacle => new ObjectiveBinnacleValueObject
+                      {
+                          Id = objectiveBinnacle.Id,
+                          EvaluationMeasuredQuestionId = objectiveBinnacle.EvaluationMeasuredQuestionId,
+                          Text = objectiveBinnacle.Text,
+                          CreationTime = objectiveBinnacle.CreationTime
+                      }).ToList(),
+                      isNotEvaluable = true
+                  }).ToListAsync();
         }
     }
 }
