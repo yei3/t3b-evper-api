99cf28956c2978ec8048d25a789a68e67e641faa
edgeroy <edgeroy@p1stache.com>
2019-02-11 00:05:57 -0500

    fix

--- src/Yei3.PersonalEvaluation.Application/Binnacle/BinnacleAppService.cs
+++ src/Yei3.PersonalEvaluation.Application/Binnacle/BinnacleAppService.cs
@@ -1,10 +1,13 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Abp.Application.Services;
+using Abp.Domain.Entities;
 using Abp.Domain.Repositories;
+using Abp.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Yei3.PersonalEvaluation.Authorization.Users;
 using Yei3.PersonalEvaluation.Binnacle.Dto;
+using Yei3.PersonalEvaluation.Evaluations.EvaluationAnswers;
 using Yei3.PersonalEvaluation.Evaluations.EvaluationQuestions;
 
 namespace Yei3.PersonalEvaluation.Binnacle
@@ -13,6 +16,7 @@ namespace Yei3.PersonalEvaluation.Binnacle
     {
 
         private readonly UserManager _userManager;
+        private readonly IRepository<NotEvaluableAnswer, long> _notEvaluableAnswerRepository;
 
         public BinnacleAppService(IRepository<ObjectiveBinnacle, long> repository, UserManager userManager) : base(repository)
         {
@@ -32,11 +36,39 @@ namespace Yei3.PersonalEvaluation.Binnacle
             ObjectiveBinnacle binnacle = await Repository
                 .GetAll()
                 .Include(objectiveBinnacle => objectiveBinnacle.EvaluationQuestion)
+                .ThenInclude(question => question.Evaluation)
                 .SingleAsync(objectiveBinnacle => objectiveBinnacle.Id == binnacleDto.Id);
 
-            binnacle.EvaluationQuestion.Status = binnacle.EvaluationQuestion.Status < EvaluationQuestionStatus.Initiated
+            binnacle.EvaluationQuestion.Status = binnacle.EvaluationQuestion.Status == EvaluationQuestionStatus.NoStatus || binnacle.EvaluationQuestion.Status == EvaluationQuestionStatus.Unanswered
                 ? EvaluationQuestionStatus.Initiated
                 : binnacle.EvaluationQuestion.Status;
+
+            NotEvaluableAnswer pairAnswer = null;
+            NotEvaluableAnswer currentEvaluableAnswer = binnacle.EvaluationQuestion.Evaluation is NotEvaluableAnswer
+                ? binnacle.EvaluationQuestion.Evaluation.As<NotEvaluableAnswer>()
+                : null;
+
+            if (!currentEvaluableAnswer.IsNullOrDeleted())
+            {
+                pairAnswer = _notEvaluableAnswerRepository
+                    .GetAll()
+                    .Include(answer => answer.NotEvaluableQuestion)
+                    .ThenInclude(question => question.Evaluation)
+                    .Where(answer => answer.Text == currentEvaluableAnswer.Text)
+                    .Where(answer => answer.Id != currentEvaluableAnswer.Id)
+                    .Where(answer => answer.NotEvaluableQuestion.Evaluation.Term == currentEvaluableAnswer.NotEvaluableQuestion.Evaluation.Term)
+                    .SingleOrDefault(answer => answer.NotEvaluableQuestion.Evaluation.UserId ==
+                                               currentEvaluableAnswer.NotEvaluableQuestion.Evaluation.UserId);
+
+            }
+
+            if (pairAnswer.IsNullOrDeleted())
+            {
+                return binnacleDto;
+            }
+
+            input.EvaluationQuestionId = pairAnswer.EvaluationQuestionId;
+            await base.Create(input);
 
             return binnacleDto;
         }
