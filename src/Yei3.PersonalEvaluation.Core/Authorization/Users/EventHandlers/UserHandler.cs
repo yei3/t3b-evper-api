3b332e9c456c3cbe1c90a0739a52120059b5f50b
epgeroy <epgeroy@gmail.com>
2019-06-15 22:34:00 -0500

    enable reassign supervisor feature on data acquisition

--- src/Yei3.PersonalEvaluation.Core/Authorization/Users/EventHandlers/UserHandler.cs
+++ src/Yei3.PersonalEvaluation.Core/Authorization/Users/EventHandlers/UserHandler.cs
@@ -8,7 +8,10 @@ using Yei3.PersonalEvaluation.Evaluations.EvaluationRevisions;
 
 namespace Yei3.PersonalEvaluation.Core.Authorization.Users.EventHandlers
 {
-    public class UserHandler : IEventHandler<EntityDeletingEventData<User>>, ITransientDependency
+    public class UserHandler :
+        IEventHandler<EntityDeletingEventData<User>>,
+        IEventHandler<EntityChangedEventData<User>>,
+        ITransientDependency
     {
 
         private readonly UserManager _userManager;
@@ -17,20 +20,36 @@ namespace Yei3.PersonalEvaluation.Core.Authorization.Users.EventHandlers
         public UserHandler(
             UserManager userManager,
             IRepository<EvaluationRevision, long> evaluationRevisionRepository
-        ) {
+        )
+        {
             _userManager = userManager;
             _evaluationRevisionRepository = evaluationRevisionRepository;
         }
 
         public void HandleEvent(EntityDeletingEventData<User> eventData)
         {
+            UpdateSupervisorSubordinateRelation(eventData.Entity);
+        }
+
+        public void HandleEvent(EntityChangedEventData<User> eventData)
+        {
+            if (eventData.Entity.IsActive)
+            {
+                return;
+            }
+
+            UpdateSupervisorSubordinateRelation(eventData.Entity);
+        }
+
+        private void UpdateSupervisorSubordinateRelation(User eventUser)
+        {
             // find user immediate superior
-            User immediateSupervisor = _userManager.Users.Single(user => user.JobDescription == eventData.Entity.ImmediateSupervisor);
+            User immediateSupervisor = _userManager.Users.Single(user => user.JobDescription == eventUser.ImmediateSupervisor);
 
             // find subordinates
             IQueryable<User> subordinates = _userManager.Users
-                .Where(user => user.ImmediateSupervisor == eventData.Entity.JobDescription);
-            
+                .Where(user => user.ImmediateSupervisor == eventUser.JobDescription);
+
             // update to new immediate supervisor
             foreach (User subordinate in subordinates)
             {
@@ -40,8 +59,8 @@ namespace Yei3.PersonalEvaluation.Core.Authorization.Users.EventHandlers
             // find dandling revisions
             IQueryable<EvaluationRevision> pendingRevisions = _evaluationRevisionRepository
                 .GetAll()
-                .Where(evaluationRevision => evaluationRevision.ReviewerUserId == eventData.Entity.Id);
-            
+                .Where(evaluationRevision => evaluationRevision.ReviewerUserId == eventUser.Id);
+
             // update new reviewer
             foreach (EvaluationRevision pendingRevision in pendingRevisions)
             {
