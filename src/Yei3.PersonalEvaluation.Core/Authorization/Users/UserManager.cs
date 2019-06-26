7a070728cef00af56fb9ecc4e90a57082690e361
epgeroy <epgeroy@gmail.com>
2019-06-26 16:47:33 -0500

    add is user salesman biz rule

--- src/Yei3.PersonalEvaluation.Core/Authorization/Users/UserManager.cs
+++ src/Yei3.PersonalEvaluation.Core/Authorization/Users/UserManager.cs
@@ -1,4 +1,5 @@
-﻿namespace Yei3.PersonalEvaluation.Authorization.Users
+﻿using System.Globalization;
+namespace Yei3.PersonalEvaluation.Authorization.Users
 {
 
     using System;
@@ -16,44 +17,47 @@
     using Yei3.PersonalEvaluation.Authorization.Roles;
     using Microsoft.EntityFrameworkCore;
     using System.Threading.Tasks;
+    using System.Linq;
+    using Yei3.PersonalEvaluation;
+    using Yei3.PersonalEvaluation.Core;
 
     public class UserManager : AbpUserManager<Role, User>
     {
         public UserManager(
             RoleManager roleManager,
-            UserStore store, 
-            IOptions<IdentityOptions> optionsAccessor, 
-            IPasswordHasher<User> passwordHasher, 
-            IEnumerable<IUserValidator<User>> userValidators, 
+            UserStore store,
+            IOptions<IdentityOptions> optionsAccessor,
+            IPasswordHasher<User> passwordHasher,
+            IEnumerable<IUserValidator<User>> userValidators,
             IEnumerable<IPasswordValidator<User>> passwordValidators,
-            ILookupNormalizer keyNormalizer, 
-            IdentityErrorDescriber errors, 
-            IServiceProvider services, 
-            ILogger<UserManager<User>> logger, 
-            IPermissionManager permissionManager, 
-            IUnitOfWorkManager unitOfWorkManager, 
-            ICacheManager cacheManager, 
-            IRepository<OrganizationUnit, long> organizationUnitRepository, 
-            IRepository<UserOrganizationUnit, long> userOrganizationUnitRepository, 
-            IOrganizationUnitSettings organizationUnitSettings, 
+            ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors,
+            IServiceProvider services,
+            ILogger<UserManager<User>> logger,
+            IPermissionManager permissionManager,
+            IUnitOfWorkManager unitOfWorkManager,
+            ICacheManager cacheManager,
+            IRepository<OrganizationUnit, long> organizationUnitRepository,
+            IRepository<UserOrganizationUnit, long> userOrganizationUnitRepository,
+            IOrganizationUnitSettings organizationUnitSettings,
             ISettingManager settingManager)
             : base(
-                roleManager, 
-                store, 
-                optionsAccessor, 
-                passwordHasher, 
-                userValidators, 
-                passwordValidators, 
-                keyNormalizer, 
-                errors, 
-                services, 
-                logger, 
-                permissionManager, 
-                unitOfWorkManager, 
+                roleManager,
+                store,
+                optionsAccessor,
+                passwordHasher,
+                userValidators,
+                passwordValidators,
+                keyNormalizer,
+                errors,
+                services,
+                logger,
+                permissionManager,
+                unitOfWorkManager,
                 cacheManager,
-                organizationUnitRepository, 
-                userOrganizationUnitRepository, 
-                organizationUnitSettings, 
+                organizationUnitRepository,
+                userOrganizationUnitRepository,
+                organizationUnitSettings,
                 settingManager)
         {
         }
@@ -61,6 +65,16 @@
         public async Task<User> FindByEmployeeNumberAsync(string employeeNumber)
         {
             return await this.Users.SingleAsync(users => users.EmployeeNumber == employeeNumber, CancellationToken);
+        }
+
+        public async Task<bool> IsUserASalesMan (User user)
+        {
+            bool isInsideOperationArea = (await GetOrganizationUnitsAsync(user))
+                .Any(organizationUnit => organizationUnit.DisplayName.Equals(BizConst.OperationsArea, StringComparison.InvariantCultureIgnoreCase));
+
+            bool hasSalesManJobDescription = BizConst.SalesManJobDescription.Contains(user.JobDescription);
+
+            return isInsideOperationArea && hasSalesManJobDescription;
         }
     }
 }
---
+++ src/Yei3.PersonalEvaluation.Core/BizConst.cs
@@ -0,0 +1,18 @@
+namespace Yei3.PersonalEvaluation.Core
+{
+    using System.Collections.ObjectModel;
+    using System.Collections.Generic;
+    public class BizConst
+    {
+        public const string OperationsArea = "Operaciones";
+        public const string StoreManagerJobDescription = "Gerente de tienda";
+        public const string DistrictManagerJobDescription = "Gerente de Distrito";
+        public const string ZoneManagerJobDescription = "Gerente de Zona";
+        public const string RegionManagerJobDescription = "Director Regional";
+
+        public static readonly ReadOnlyCollection<string> SalesManJobDescription = new ReadOnlyCollection<string>(new List<string>
+        {
+            StoreManagerJobDescription, DistrictManagerJobDescription, ZoneManagerJobDescription, RegionManagerJobDescription
+        });
+    }
+}
