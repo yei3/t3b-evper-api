6ae44acff44f1daa63a8c77e23e7d315bd159925
epgeroy <epgeroy@gmail.com>
2019-07-07 23:58:45 -0500

    clean unused imports

--- src/Yei3.PersonalEvaluation.Application/Users/UserAppService.cs
+++ src/Yei3.PersonalEvaluation.Application/Users/UserAppService.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Mail;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
