c1c2369b7660dfac759de2ca07589437d0e690af
epgeroy <epgeroy@gmail.com>
2019-07-19 17:24:46 -0500

    minor fix

--- src/Yei3.PersonalEvaluation.Application/Evaluations/Dto/EvaluaktionMeasuredQuestionDto.cs
+++ src/Yei3.PersonalEvaluation.Application/Evaluations/Dto/EvaluaktionMeasuredQuestionDto.cs
@@ -1,10 +1,11 @@
+using Abp.Application.Services.Dto;
 using Abp.AutoMapper;
 using Yei3.PersonalEvaluation.Evaluations.EvaluationQuestions;
 
 namespace Yei3.PersonalEvaluation.Evaluations.Dto
 {
     [AutoMap(typeof(EvaluationMeasuredQuestion))]
-    public class EvaluationMeasuredQuestionDto
+    public class EvaluationMeasuredQuestionDto : EntityDto<long>
     {
         public decimal? Expected { get; set; }
         public string ExpectedText { get; set; }
