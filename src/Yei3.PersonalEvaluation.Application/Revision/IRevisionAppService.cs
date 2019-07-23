e5c3f20d830863f51b8718884ca60b65e245fa4a
luiarhs <luisangel@yei3.com>
2019-07-22 22:47:37 -0500

    [FIX] remove UnreviseEvaluation interface

--- src/Yei3.PersonalEvaluation.Application/Revision/IRevisionAppService.cs
+++ src/Yei3.PersonalEvaluation.Application/Revision/IRevisionAppService.cs
@@ -6,7 +6,6 @@ namespace Yei3.PersonalEvaluation.Revision
     public interface IRevisionAppService
     {
         Task ReviseEvaluation(long evaluationId);
-        Task UnreviseEvaluation(long evaluationId);
         Task FinishEvaluation(long evaluationId);
         Task UnfininshEvaluation(long evaluationId);
         Task UpdateRevisionDate(UpdateRevisionDateInputDto input);
