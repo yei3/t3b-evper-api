4763081aad55550f95c4fae405ebab8c789e2f73
epgeroy <epgeroy@gmail.com>
2019-08-31 13:30:04 -0500

    rename controller

---
+++ src/Yei3.PersonalEvaluation.Web.Host/Controllers/ImportUserController.cs
@@ -0,0 +1,79 @@
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using Yei3.PersonalEvaluation.Authorization.Users;
+using Yei3.PersonalEvaluation.Controllers;
+
+namespace Yei3.PersonalEvaluation.Web.Host.Controllers
+{
+    [Route("api/[controller]")]
+    public class ImportUserController : PersonalEvaluationControllerBase
+    {
+
+        private readonly UserRegistrationManager _userRegistrationManager;
+
+        public ImportUserController(UserRegistrationManager userRegistrationManager)
+        {
+            this._userRegistrationManager = userRegistrationManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImportUserAction([FromForm]IFormFile file)
+        {
+            string filePath = Path.GetTempFileName();
+            
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 2; row <= rowCount; row++) // start in row 3 cause data starts there, any template change can break this, is better if we provide the template
+                {
+                    try
+                    {
+                        User currentUser = _userRegistrationManager.ImportUser(
+                            employeeNumber: worksheet.Cells[row, 1].Value.ToString(),
+                            status: worksheet.Cells[row, 2].Value.ToString() == "ACTIVO",
+                            firstLastName: worksheet.Cells[row, 3].Value.ToString(),
+                            secondLastName: worksheet.Cells[row, 4].Value?.ToString(),
+                            name: worksheet.Cells[row, 5].Value.ToString(),
+                            jobDescription: worksheet.Cells[row, 6].Value.ToString(),
+                            area: worksheet.Cells[row, 7].Value.ToString(),
+                            region: worksheet.Cells[row, 8].Value.ToString(),
+                            immediateSupervisor: worksheet.Cells[row, 9].Value.ToString(),
+                            socialReason: worksheet.Cells[row, 10].Value.ToString(),
+                            isSupervisor: worksheet.Cells[row, 11].Value != null && worksheet.Cells[row, 11].Value.ToString().Contains('X'),
+                            isManager: worksheet.Cells[row, 12].Value != null && worksheet.Cells[row, 12].Value.ToString().Contains('X'),
+                            entryDate: worksheet.Cells[row, 13].Value.ToString(),
+                            reassignDate: worksheet.Cells[row, 14].Value == null
+                                ? null
+                                : worksheet.Cells[row, 14].Value.ToString(),
+                            birthDate: worksheet.Cells[row, 15].Value?.ToString(),
+                            scholarship: worksheet.Cells[row, 16].Value.ToString(),
+                            email: worksheet.Cells[row, 17].Value.ToString(),
+                            isMale: worksheet.Cells[row, 18].Value.ToString() == "MASCULINO"
+                        );
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.Info(e.Message);
+                        Logger.Info($"Usuario {worksheet.Cells[row, 3].Value} fue agregado anteriormente.");
+                    }
+                }
+
+                return Ok();
+            }
+        }
+    }
+}
