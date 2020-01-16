c35cd3816ed5de1fe10d102ed5d691d718a65559
epgeroy <epgeroy@gmail.com>
2020-01-16 01:57:05 -0600

    add download file from cache feature

---
+++ src/Yei3.PersonalEvaluation.Web.Core/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+using Abp.Auditing;
+using Abp.Runtime.Caching;
+using Microsoft.AspNetCore.Mvc;
+using Yei3.PersonalEvaluation.Evaluations.Dto;
+
+namespace Yei3.PersonalEvaluation.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class FileController : PersonalEvaluationControllerBase
+    {
+        private readonly ICacheManager CacheManager;
+
+        public FileController(ICacheManager cacheManager)
+        {
+            CacheManager = cacheManager;
+        }
+
+        [DisableAuditing]
+        [HttpGet]
+        public ActionResult DownloadTempFile(FileDto file)
+        {
+            if (!(CacheManager.GetCache(AppConsts.TempEvaluationStatusesFileName).Get(file.FileToken, ep => ep) is byte[] fileBytes))
+            {
+                return NotFound(L("RequestedFileDoesNotExists"));
+            }
+
+            return File(fileBytes, file.FileType, file.FileName);
+        }
+    }
+}
